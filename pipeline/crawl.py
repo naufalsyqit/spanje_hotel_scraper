b622d7feb4e9e1cd4008d9cd717fef89c78c2fc4
naufalshwki <kolok27@gmail.com>
2025-12-06 12:54:21 +0700

    add try except in the crawl part

--- pipeline/crawl.py
+++ pipeline/crawl.py
@@ -60,67 +60,73 @@ class Crawl:
                         "additional_info": "",
                     }
                 )
-                logging.info(
-                    f"Processing hotel: {hotel_name}, Departure: {departure_option['name']}, Date: {date_option['text'].strip()}"
-                )
-                hotel_info["departure_month"] = date_option["text"].strip()
-                price_payload = f"DepartureFrom={departure_option['code']}&DepartureDate={date_option['value']}&IsSale=false&Destination=Spanje&Entity=-1_{payload_extension}"
-                price_html = get_hotel_price_grid(
-                    self.pref_change_url,
-                    cookie_string,
-                    price_payload,
-                    hotel_session,
-                )
+                try:
+                    logging.info(
+                        f"Processing hotel: {hotel_name}, Departure: {departure_option['name']}, Date: {date_option['text'].strip()}"
+                    )
+                    hotel_info["departure_month"] = date_option["text"].strip()
+                    price_payload = f"DepartureFrom={departure_option['code']}&DepartureDate={date_option['value']}&IsSale=false&Destination=Spanje&Entity=-1_{payload_extension}"
+                    price_html = get_hotel_price_grid(
+                        self.pref_change_url,
+                        cookie_string,
+                        price_payload,
+                        hotel_session,
+                    )
 
-                if not price_html:
+                    if not price_html:
+                        hotel_info["additional_info"] = "No price grid found"
+                        hotel_data.append(hotel_info.copy())
+                        logging.info(
+                            f"Failed to fetch price grid for {hotel_name} with departure {departure_option['name']} anda date {date_option['text']}"
+                        )
+                        continue
+
+                    # 2. Parse with BeautifulSoup
+                    decoded_price_soup = decode_and_soup(price_html)
+                    lowest_price_tag = decoded_price_soup.find("span", string="Laagste")
+
+                    if not lowest_price_tag:
+                        hotel_info["additional_info"] = "No trips found"
+                        hotel_data.append(hotel_info.copy())
+                        logging.info(
+                            f"No lowest price found for {hotel_name} with departure {departure_option['name']}"
+                        )
+                        continue
+
+                    lowest_price_button = lowest_price_tag.find_parent("button")
+                    lowest_price = (
+                        lowest_price_button.get_text().replace("Laagste", "").strip()
+                    )
+                    price_selection_id = lowest_price_button["rev"]
+                    selected_price = f"{lowest_price}+Laagste"
+                    selected_price_payload = f"PriceSelectionId={price_selection_id}&SelectedPrice={selected_price}&IsPricePerPerson=True&IsSale=false&Destination=Spanje&Entity=-1_{payload_extension}"
+                    price_details_json = get_hotel_price_details(
+                        self.price_select_url,
+                        cookie_string,
+                        selected_price_payload,
+                        hotel_session,
+                    )
+
+                    trip_options = get_trip_options(price_details_json)
+                    hotel_info.update(trip_options)
+                    hotel_data.append(hotel_info.copy())
+
+                    # debug line
+                    # with open(f"output/raw_json/{hotel_name}_{departure_option['code']}_price_details.json", "w", encoding="utf-8") as file:
+                    #     json.dump(price_details_json, file, indent=4)
+                    # with open(f"output/raw_html/{hotel_name}_{departure_option['code']}_{date_option['text'].strip()}.html", "w", encoding="utf-8") as file:
+                    #     file.write(decoded_price_soup.prettify())
+                    # with open(f"output/txt_output/{hotel_name}_{departure_option['code']}_{date_option['text'].strip()}.txt", "w", encoding="utf-8") as file:
+                    #     file.write(f"Hotel: {hotel_name}, Departure: {departure_option['code']}, Lowest Price: {lowest_price if lowest_price else 'N/A'}, Price Selection ID: {price_selection_id}, selected_price: {selected_price}")
+
+                    logging.info(
+                        f"Hotel: {hotel_name}, Departure: {departure_option['code']}, Lowest Price: {lowest_price if lowest_price else 'N/A'}, Price Selection ID: {price_selection_id}, selected_price: {selected_price}"
+                    )
+                except Exception as e:
+                    logging.error(
+                        f"Error processing hotel {hotel_name} with departure {departure_option['name']} and date {date_option['text']}: {e}"
+                    )
                     hotel_info["additional_info"] = "No price grid found"
                     hotel_data.append(hotel_info.copy())
-                    logging.info(
-                        f"Failed to fetch price grid for {hotel_name} with departure {departure_option['name']} anda date {date_option['text']}"
-                    )
-                    # print(response.text)
-                    continue
-
-                # 2. Parse with BeautifulSoup
-                decoded_price_soup = decode_and_soup(price_html)
-                lowest_price_tag = decoded_price_soup.find("span", string="Laagste")
-
-                if not lowest_price_tag:
-                    hotel_info["additional_info"] = "No trips found"
-                    hotel_data.append(hotel_info.copy())
-                    logging.info(
-                        f"No lowest price found for {hotel_name} with departure {departure_option['name']}"
-                    )
-                    continue
-
-                lowest_price_button = lowest_price_tag.find_parent("button")
-                lowest_price = (
-                    lowest_price_button.get_text().replace("Laagste", "").strip()
-                )
-                price_selection_id = lowest_price_button["rev"]
-                selected_price = f"{lowest_price}+Laagste"
-                selected_price_payload = f"PriceSelectionId={price_selection_id}&SelectedPrice={selected_price}&IsPricePerPerson=True&IsSale=false&Destination=Spanje&Entity=-1_{payload_extension}"
-                price_details_json = get_hotel_price_details(
-                    self.price_select_url,
-                    cookie_string,
-                    selected_price_payload,
-                    hotel_session,
-                )
-
-                trip_options = get_trip_options(price_details_json)
-                hotel_info.update(trip_options)
-                hotel_data.append(hotel_info.copy())
-
-                # debug line
-                # with open(f"output/raw_json/{hotel_name}_{departure_option['code']}_price_details.json", "w", encoding="utf-8") as file:
-                #     json.dump(price_details_json, file, indent=4)
-                # with open(f"output/raw_html/{hotel_name}_{departure_option['code']}_{date_option['text'].strip()}.html", "w", encoding="utf-8") as file:
-                #     file.write(decoded_price_soup.prettify())
-                # with open(f"output/txt_output/{hotel_name}_{departure_option['code']}_{date_option['text'].strip()}.txt", "w", encoding="utf-8") as file:
-                #     file.write(f"Hotel: {hotel_name}, Departure: {departure_option['code']}, Lowest Price: {lowest_price if lowest_price else 'N/A'}, Price Selection ID: {price_selection_id}, selected_price: {selected_price}")
-
-                logging.info(
-                    f"Hotel: {hotel_name}, Departure: {departure_option['code']}, Lowest Price: {lowest_price if lowest_price else 'N/A'}, Price Selection ID: {price_selection_id}, selected_price: {selected_price}"
-                )
 
         return hotel_data
